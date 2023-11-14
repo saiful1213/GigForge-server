4a59d49b02d1d29ffe63e6e08933a22543da37ac
saiful-islam <md2215566@gmail.com>
2023-11-14 13:23:50 +0600

    final commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-
 // middlewares
 app.use(express.json())
 app.use(cors())
@@ -14,7 +13,6 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8d3p09.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
    serverApi: {
       version: ServerApiVersion.v1,
@@ -41,7 +39,7 @@ async function run() {
       })
 
       // post bidder info
-      app.post('/api/v1/bid-info', async (req, res) => {
+      app.post('/api/v1/bid_info', async (req, res) => {
          const info = req.body;
          const result = await bidCollection.insertOne(info);
          res.send(result);
@@ -90,6 +88,20 @@ async function run() {
          res.send(result);
       })
 
+      // get data for my_bids page
+      app.get('/api/v1/bid_info/:email', async (req, res) => {
+         const email = req.params.email;
+         const query = { bidderEmail: email }
+         const result = await bidCollection.find(query).toArray();
+         res.send(result);
+      })
+
+      // get data for bid-request page
+      app.get('/api/v1/bid_info', async (req, res) => {
+         const result = await bidCollection.find().toArray();
+         res.send(result);
+      })
+
 
 
       // update data
@@ -108,8 +120,16 @@ async function run() {
       })
 
 
+      // delete data
+      app.delete('/api/v1/jobs/delete/:id', async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) }
+         const result = await jobCollection.deleteOne(filter);
+         res.send(result)
+      })
 
-      // Connect the client to the server	(optional starting in v4.7)
+
+
       // await client.connect();
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
@@ -120,7 +140,6 @@ async function run() {
    }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req, res) => {
